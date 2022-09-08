34aa3704536790a59f6380a1559289f20b2e49fc
kobakichi <fenderstrat57vin@gmail.com>
2022-09-09 04:50:34 +0900

    add README.mdの内容追加

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Next.js と TypeScript でのモーダルコンポーネントの作成。
+# Next.js ModalPractice Project
+
+React のフレームワークである Next.js を利用して、TailwindCSS、HeadlessUI を利用した Modal を作成します。
+
+## 使用した技術
+
+- Next.js
+- TailwindCSS
+- HeadlessUI
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Modal from "../components/Modal";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Modal from '../components/Modal';
 
 const Home: NextPage = () => {
   return (
